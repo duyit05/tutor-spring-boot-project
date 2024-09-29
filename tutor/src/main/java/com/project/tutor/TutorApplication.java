dedc3d5f55725b7e680248402c3a54ba8662e599
DuyCoder <duynv.it.052@gmail.com>
2024-09-29 12:14:10 +0700

    update-logger

--- tutor/src/main/java/com/project/tutor/TutorApplication.java
+++ tutor/src/main/java/com/project/tutor/TutorApplication.java
@@ -1,10 +1,12 @@
 package com.project.tutor;
 
+import lombok.extern.flogger.Flogger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@Flogger
 public class TutorApplication {
 
 	public static void main(String[] args) {
